b4ddb3cc61529be6adafa08f825f4d79d03c62c6
Misganaw-Berihun <misganawbmb@gmail.com>
2024-03-25 06:52:33 +0300

    initial setup

---
+++ front-end/src/App.jsx
@@ -0,0 +1,10 @@
+import EditableCanvas from "./components/EditableCanvas";
+import JsonFileUploader from "./components/JsonFileUploader";
+
+export default function App() {
+  return (
+    <div>
+      <EditableCanvas/>
+    </div>
+  )
+}
